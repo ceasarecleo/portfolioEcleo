ac4faa8c15521cf6beb7bf826524c32d6f7ae869
ceasarecleo <100175797+ceasarecleo@users.noreply.github.com>
2022-06-12 11:09:16 -1200

    Add files via upload

---
+++ MyPortfolioEcleo/js/script.js
@@ -0,0 +1,7 @@
+/*typing animation*/
+var typed = new Typed(".typing", {
+    strings:["", "Programmer", "Debugger","Student", "Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
